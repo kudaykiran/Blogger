92557e57f23966c799703ba59fccd2672c691a18
AKSAHA <sheol.aks@gmail.com>
2020-04-11 05:28:46 +0100

    Final Commit

---
+++ src/build/precache-manifest.b511f2b6f704a22c352a9a8a46fc0844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d1f31f92dac2ac76691f1ca078b20b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05cb9ecc898e0c4d494c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e3e636c3bc11ba51402",
+    "url": "/static/js/2.d719da6e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d719da6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05cb9ecc898e0c4d494c",
+    "url": "/static/js/main.fc301815.chunk.js"
+  },
+  {
+    "revision": "729c0e34abf8ca650159",
+    "url": "/static/js/runtime-main.f64ebbce.js"
+  }
+]);
